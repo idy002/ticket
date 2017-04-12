1c61e82ea5d15cf1de3762c3bdf8030d7706e991
idy002 <idy002@163.com>
2017-04-12 14:17:06 +0800

    add shared_ptr

---
+++ code/lib/shared_ptr.hpp
@@ -0,0 +1,21 @@
+/*
+ * shared_ptr.h - implement the shared_ptr template
+ */
+#ifndef TICKET_MAP_H
+#define TICKET_MAP_H
+
+template<typename T>
+class shared_ptr {
+	size_t * count;
+	T * ptr;
+	public:
+	shared_ptr( T * ptr = 0 ):ptr(ptr),count(new size_t(0)) {
+		if( ptr )
+			count = 1;
+		else
+			count = 0;
+	}
+
+};
+
+#endif
