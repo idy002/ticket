a5b3c599ea0280552b22dad575a56a5a3771f1d1
idy002 <idy002@163.com>
2017-04-29 16:40:16 +0800

    modified shared_ptr

---
+++ code/lib/exception.hpp
@@ -0,0 +1,10 @@
+/*
+ * exception.hpp - declare some exceptions
+ */
+#ifndef TICKET_EXCEPTION_H
+#define TICKET_EXCEPTION_H
+
+class bad_pointer {};
+
+#endif
+
--- code/lib/shared_ptr.hpp
+++ code/lib/shared_ptr.hpp
@@ -4,16 +4,44 @@
 #ifndef TICKET_MAP_H
 #define TICKET_MAP_H
 
+#include "exception.hpp"
+
 template<typename T>
 class shared_ptr {
 	size_t * count;
 	T * pointer;
+	void release() {
+		if( --(*count) == 0 ) {
+			delete pointer;
+			delete count;
+		}
+	}
 	public:
-	shared_ptr( T * ptr = 0 ):ptr(ptr),count(new size_t(0)) {
-		if( ptr )
-			*count = 1;
-		else
-			*count = 0;
+	shared_ptr( T * p = 0 ):pointer(p),count(new size_t(1)) { }
+	shared_ptr( const shared_ptr & rhs ) {
+		if( this == &rhs ) return;
+		count = rhs.count;
+		pointer = rhs.pointer;
+		(*count)++;
+	}
+	shared_ptr & operator=( const shared_ptr & rhs ) {
+		if( this == &rhs ) return *this;
+		release();
+		pointer = rhs.pointer;
+		count = rhs.count;
+		(*count)++;
+		return *this;
+	}
+	T & operator*() {
+		if( pointer ) return *pointer;
+		throw bad_pointer();
+	}
+	T * operator->() {
+		if( pointer ) return pointer;
+		throw bad_pointer();
+	}
+	~shared_ptr() {
+		release();
 	}
 };
 
